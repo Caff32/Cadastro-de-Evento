a47a9719405dbbf8eee9212d9d464db7eac3a503
Caff32 <carlosalbertoff@outlook.com>
2023-10-24 23:17:03 -0300

    Estrutura do Projeto

--- src/main/java/com/work/event/EventApplication.java
+++ src/main/java/com/work/event/EventApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EventApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EventApplication.class, args);
+		SpringApplication.run(EventApplication.class, args );
 	}
 
 }
---
+++ src/main/java/com/work/event/model/Event.java
@@ -0,0 +1,19 @@
+package com.work.event.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Event {
+
+    private long id;
+    private String title;
+    private String description;
+    private Date date;
+    private String local;
+    private String maxpeople;
+}
